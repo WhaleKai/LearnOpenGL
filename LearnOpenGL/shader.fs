611de7ab875dace6eb17a84a4344f9e463728644
asus <1481361244@qq.com>
2024-09-29 16:12:58 +0800

    Revert "1.8.1"
    This reverts commit f81096d2b81d10ba7c30a5e5594a4fbadd3366b2.

---
+++ LearnOpenGL/shader.fs
@@ -0,0 +1,15 @@
+#version 330 core
+out vec4 FragColor;
+
+//in vec3 ourColor;
+in vec2 TexCoord;
+
+uniform sampler2D texture1;	// 输入读取纹理
+uniform sampler2D texture2;
+
+void main()
+{
+	//FragColor = vec4(ourColor, 1.0f);	//用顶点着色器的颜色设置顶点颜色
+	//FragColor = texture(ourTexture, TexCoord) * vec4(ourColor, 1.0f);	//贴图
+	FragColor = mix(texture(texture1, TexCoord), texture(texture2, TexCoord), 0.2);	//贴图
+}
---
+++ LearnOpenGL/源.cpp
@@ -0,0 +1,24 @@
+//#include <glad/glad.h>
+//#include <GLFW/glfw3.h>
+//
+//#include "shader_s.h"
+//#define STB_IMAGE_IMPLEMENTATION
+//#include "stb_image.h"
+//
+//#include <glm/glm.hpp>
+//#include <glm/gtc/matrix_transform.hpp>
+//#include <glm/gtc/type_ptr.hpp>
+//
+//#include <iostream>
+//
+//int main()
+//{
+//	glm::vec4 vec(1.0f, 0.0f, 0.0f, 1.0f);
+//	glm::mat4 trans;
+//	trans = glm::translate(trans, glm::vec3(1.0f, 1.0f, 0.0f));
+//	vec = trans * vec;
+//	std::cout << vec.x << vec.y << vec.z << std::endl;
+//
+//	system("pause");
+//	return 0;
+//}
