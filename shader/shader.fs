458d6b13954e0e7d70e76a3fb6ad80551997744d
asus <1481361244@qq.com>
2024-10-06 15:15:06 +0800

    2.4.1 平行光

--- shader/shader.fs
+++ shader/shader.fs
@@ -12,7 +12,8 @@ struct Material {
     float shininess;
 }; 
 struct Light {
-    vec3 position;
+    //vec3 position;
+	vec3 direction;
 
     vec3 ambient;
     vec3 diffuse;
@@ -36,7 +37,8 @@ void main()
 
 	// Âþ·´Éä
 	vec3 norm = normalize(Normal);
-	vec3 lightDir = normalize(light.position - FragPos);
+	// vec3 lightDir = normalize(light.position - FragPos);
+	vec3 lightDir = normalize(-light.direction);
 	float diff = max(dot(norm, lightDir), 0.0);
 	vec3 diffuse = light.diffuse * diff * vec3(texture(material.diffuse, TexCoords));
 
